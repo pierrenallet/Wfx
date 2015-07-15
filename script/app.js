dc8c69e9da7b61832438d80d4b534ca0d6d99831
Pierre Nallet <pierrenallet@hotmail.com>
2015-07-14 23:24:24 -0700

    Adds first code viewer in text area

---
+++ script/app.js
@@ -0,0 +1,50 @@
+var Web;
+(function (Web) {
+    var MiniPromise = (function () {
+        function MiniPromise(action) {
+            var _this = this;
+            action(function (r) { return _this._then(r); }, function (e) { return console.log("Error"); });
+        }
+        MiniPromise.prototype.then = function (action) {
+            this._then = action;
+        };
+        return MiniPromise;
+    })();
+    Web.MiniPromise = MiniPromise;
+    var WebClient = (function () {
+        function WebClient() {
+        }
+        WebClient.prototype.execute = function (url, verb) {
+            if (verb === void 0) { verb = "GET"; }
+            return new MiniPromise(function (resolve, reject) {
+                var request = new XMLHttpRequest();
+                request.open(verb, url, true);
+                request.onreadystatechange = function (ev) {
+                    if (request.readyState === 4) {
+                        if (request.status === 200) {
+                            resolve(request.response);
+                        }
+                        else {
+                            reject("Error: " + request.status);
+                        }
+                    }
+                };
+                request.send();
+            });
+        };
+        WebClient.prototype.get = function (url) {
+            return this.execute(url);
+        };
+        return WebClient;
+    })();
+    Web.WebClient = WebClient;
+})(Web || (Web = {}));
+setTimeout(function () {
+    var items = document.querySelectorAll(".code");
+    for (var i = 0; i < items.length; i++) {
+        var item = items.item(i);
+        var url = item.attributes.getNamedItem("data-url").value;
+        var webclient = new Web.WebClient();
+        webclient.get(url).then(function (s) { return item.innerText = s; });
+    }
+}, 0.1);
---
+++ script/app.ts
@@ -0,0 +1,44 @@
+module Web {
+	export class MiniPromise<T>
+	{
+		constructor(action: (resolve, reject) => void) {
+			action((r) => this._then(r), (e) => console.log("Error"));
+
+		}
+		_then: (t: T) => void;
+		then(action: (t: T) => void) {
+			this._then = action;
+		}
+	}
+    export class WebClient {
+        execute(url: string, verb: string = "GET"): MiniPromise<string> {
+            return new MiniPromise<string>((resolve, reject) => {
+                var request = new XMLHttpRequest();
+                request.open(verb, url, true);
+                request.onreadystatechange = ev => {
+                    if (request.readyState === 4) {
+                        if (request.status === 200) {
+                            resolve(request.response);
+                        } else {
+                            reject("Error: " + request.status);
+                        }
+                    }
+                }
+                request.send();
+            });
+        }
+        get(url): MiniPromise<string> {
+            return this.execute(url);
+        }
+    }
+}
+setTimeout(() => {
+	var items = document.querySelectorAll(".code");
+	for (var i = 0; i < items.length; i++) {
+		var item = <HTMLTextAreaElement>items.item(i);
+		var url = item.attributes.getNamedItem("data-url").value;
+		var webclient = new Web.WebClient();
+		webclient.get(url).then(s => item.innerText = s); 
+	}
+}
+	, 0.1);
